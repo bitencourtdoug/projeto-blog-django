3385dc2bffc9db163ec50308b5eecf2712db0e9e
Douglas <douglasbitencourtadm@gmail.com>
2024-06-18 18:12:21 -0300

    Error 404

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import index, post, page, created_by, category
+from blog.views import index, post, page, created_by, category, tag, search
 
 app_name = 'blog'
 
@@ -10,5 +10,7 @@ urlpatterns = [
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/', created_by, name='created_by'),
     path('category/<slug:slug>/', category, name='category'),
+    path('tag/<slug:slug>/', tag, name='tag'),
+    path('search/', search, name='search'),
 ]
 
